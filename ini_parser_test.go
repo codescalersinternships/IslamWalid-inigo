febfa2d65e5d4566b6ada047d042885f4d96e355
IslamWalid <islamwalid83@gmail.com>
2022-07-19 16:57:09 +0200

    add examples

--- ini_parser_test.go
+++ ini_parser_test.go
@@ -1,10 +1,14 @@
 package iniparser
 
 import (
+	"fmt"
 	"path"
 	"reflect"
+	"sort"
 	"testing"
 )
+
+// Test functions
 
 func TestLoadFromFile(t *testing.T) {
     t.Run("file exists", func(t *testing.T) {
@@ -129,7 +133,113 @@ func TestSaveToFile(t *testing.T) {
     assertIniDataMap(t, got.iniDataMap, want.iniDataMap)
 }
 
-func assertString(t testing.TB, got, want string)  {
+// Example functions
+
+func ExampleNew() {
+    myParser := New()
+    // the output is empty since we did not parse any data yet
+    fmt.Println(myParser)
+    // output:
+}
+
+func ExampleParser_LoadFromFile() {
+    // Create parser1 and fill it with data parsed from ini_files/ref.ini
+    p1 := New()
+    p1.LoadFromFile(path.Join("ini_files", "ref.ini"))
+
+    // Create parser2 and fill it with data parsed from the same file
+    p2 := New()
+    p2.LoadFromFile(path.Join("ini_files", "ref.ini"))
+
+    fmt.Println(reflect.DeepEqual(p1, p2))
+    //output: true
+}
+
+func ExampleParser_LoadFromString() {
+    // Create reference parser and fill it with data parsed from ini_files/ref.ini
+    refParser := New()
+    refParser.LoadFromFile(path.Join("ini_files", "ref.ini"))
+
+    // Create generated parser from the string resulted from the reference parser
+    genParser := New()
+    genParser.LoadFromString(refParser.String())
+
+    fmt.Println(reflect.DeepEqual(refParser, genParser))
+    //output: true
+}
+
+func ExampleParser_GetSectionNames() {
+    // Create new parser and fill it with data parsed from ini_files/ref.ini
+    p := New()
+    p.LoadFromFile(path.Join("ini_files", "ref.ini"))
+
+    sectionNames := p.GetSectionNames()
+
+    // Sort the resulting slice to always match the output example
+    sort.Strings(sectionNames)
+
+    fmt.Println(sectionNames)
+    // output: [database owner]
+}
+
+func ExampleParser_GetSections() {
+    // Create parser1 and fill it with data parsed from ini_files/ref.ini
+    p1 := New()
+    p1.LoadFromFile(path.Join("ini_files", "ref.ini"))
+
+    // Create parser2 and fill it with data parsed from the same file
+    p2 := New()
+    p2.LoadFromFile(path.Join("ini_files", "ref.ini"))
+
+    fmt.Println(reflect.DeepEqual(p1, p2))
+    // output: true
+}
+
+func ExampleParser_Get() {
+    // Create new parser and fill it with data parsed from ini_files/ref.ini
+    p := New()
+    p.LoadFromFile(path.Join("ini_files", "ref.ini"))
+
+    valueField, _ := p.Get("owner", "name")
+    fmt.Println(valueField)
+    // output: John Doe
+}
+
+func ExampleParser_Set() {
+    // Create new parser and fill it with data parsed from ini_files/ref.ini
+    p := New()
+    p.LoadFromFile(path.Join("ini_files", "ref.ini"))
+
+    // Sets the entity with key "name" in section "owner" to value "person"
+    p.Set("owner", "name", "person")
+    valueField, _ := p.Get("owner", "name")
+    fmt.Println(valueField)
+    // output: person
+}
+
+func ExampleParser_String() {
+    // Create new parser and fill it with data parsed from ini_files/ref.ini
+    p := New()
+    p.LoadFromFile(path.Join("ini_files", "ref.ini"))
+
+    // Parse implements String so it is printed with ini form
+    fmt.Println(p)
+}
+
+func ExampleParser_SaveToFile() {
+    // Create new parser
+    p := New()
+
+    // Add entity with key "new key", value "new value" in section "new section"
+    p.Set("new section", "new key", "new value")
+
+    // Save the date to the given file in ini form
+    p.SaveToFile(path.Join("ini_files", "gen.ini"))
+}
+
+// Helper functions
+
+func assertString(t testing.TB, got, want string) {
     t.Helper()
 
     if got != want {
@@ -152,7 +262,6 @@ func assertIniDataMap(t testing.TB, got, want Section) {
         t.Errorf("got:\n%v\nwant:\n%v", got, want)
     }
 }
-
 
 func assertSectionList(t testing.TB, got, want []string) {
     t.Helper()
